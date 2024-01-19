383c174acaae390f350313e76853a7b90374f0bc
centino90 <anthonyjayansit2326282@gmail.com>
2024-01-19 22:26:12 +0800

    update postgres config

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
+      host: env('DATABASE_HOST', 'ep-dry-dust-85999105-pooler.ap-southeast-1.postgres.vercel-storage.com'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'postgres'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'password'),
+      database: env('DATABASE_NAME', 'verceldb'),
+      user: env('DATABASE_USERNAME', 'default'),
+      password: env('DATABASE_PASSWORD', 'hpaMcE3XZ9OF'),
       schema: env('DATABASE_SCHEMA', 'public')
     },
     debug: false,
